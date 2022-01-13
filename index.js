67ce48c5135144662a954cf0bbbca14326468110
RITURESH KUMAR RAI <76609761+Rituresh143@users.noreply.github.com>
2022-01-14 02:57:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+const program = require('commander')
+
+const chalk = require('chalk')
+const clipboardy = require('clipboardy')
+
+const log = console.log
+const createPassword = require('./utils/createPassword') 
+const savePassword = require('./utils/savePassword') 
+
+
+program.version('1.0.0').description('Simple Password Generator')
+
+
+program
+  .option('-l,--length <length>', 'length of password','8')
+  .option('-s,--save', 'save password to password.txt')
+  .option('-nn,--no-numbers', 'remove numbers')
+  .option('-ns,--no-symbols', 'remove symbol')
+  .parse()
+
+  const {length,save,numbers,symbols} = program.opts()
+
+//   Get generated password
+const generatedPassword = createPassword(length, numbers, symbols)
+
+
+// Save to file
+if(save){
+    savePassword(generatedPassword)
+}
+
+// Copy to Clipboardy
+clipboardy.writeSync(generatedPassword)
+
+// Output generated
+log(chalk.blue('Generated Password') + chalk.bold(generatedPassword))
+log(chalk.yellow('Password copied to clipboard'))
